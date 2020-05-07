ee953a8f0e44471d1f893ecaa538cd8db7041f01
motokikasai <kmomoky@hotmail.com>
2020-05-07 14:06:47 +0200

    Set up basic struture with Swtich, Route, NavLink & reuse logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menu: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div className="main-container">
+            <div className="nav-bar">
+              <NavLink exact to="/">
+                <div className="logo"></div>
+              </NavLink>
+              <div className="hamburger-menu">menu</div>
+            </div>
+
+            <Switch>
+              <Route exact path="/">
+                <h1>home</h1>
+              </Route>
+              <Route path="/about">
+                <h1>about</h1>
+              </Route>
+              <Route path="/project">
+                <h1>project</h1>
+              </Route>
+              <Route>
+                <h1>404</h1>
+              </Route>
+            </Switch>
+          </div>
+
+          <div className="menu-container">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink exact to="/about">
+              About
+            </NavLink>
+            <NavLink exact to="/project">
+              Project
+            </NavLink>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
