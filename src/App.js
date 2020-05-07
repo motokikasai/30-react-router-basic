bf1eda9f78a1221e32639cb592d8cdd7f4e2acb3
motokikasai <kmomoky@hotmail.com>
2020-05-07 14:27:30 +0200

    Apply menu as trigger to show menu overlay

--- src/App.js
+++ src/App.js
@@ -9,47 +9,63 @@ class App extends React.Component {
     this.state = {
       menu: false,
     };
+
+    this.menuHandler = this.menuHandler.bind(this);
+  }
+
+  menuHandler() {
+    this.setState({
+      menu: true,
+    });
   }
 
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <div className="main-container">
-            <div className="nav-bar">
-              <NavLink exact to="/">
-                <div className="logo"></div>
-              </NavLink>
-              <div className="hamburger-menu">menu</div>
+          {!this.state.menu ? (
+            <div className="main-container">
+              <div className="nav-bar">
+                <NavLink exact to="/">
+                  <div className="logo"></div>
+                </NavLink>
+
+                <div className="hamburger-menu">
+                  <button
+                    onClick={this.menuHandler}
+                    className="burger-icon"
+                  ></button>
+                </div>
+              </div>
+
+              <Switch>
+                <Route exact path="/">
+                  <h1>home</h1>
+                </Route>
+                <Route path="/about">
+                  <h1>about</h1>
+                </Route>
+                <Route path="/project">
+                  <h1>project</h1>
+                </Route>
+                <Route>
+                  <h1>404</h1>
+                </Route>
+              </Switch>
             </div>
-
-            <Switch>
-              <Route exact path="/">
-                <h1>home</h1>
-              </Route>
-              <Route path="/about">
-                <h1>about</h1>
-              </Route>
-              <Route path="/project">
-                <h1>project</h1>
-              </Route>
-              <Route>
-                <h1>404</h1>
-              </Route>
-            </Switch>
-          </div>
-
-          <div className="menu-container">
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-            <NavLink exact to="/about">
-              About
-            </NavLink>
-            <NavLink exact to="/project">
-              Project
-            </NavLink>
-          </div>
+          ) : (
+            <div className="menu-container">
+              <NavLink exact to="/">
+                Home
+              </NavLink>
+              <NavLink exact to="/about">
+                About
+              </NavLink>
+              <NavLink exact to="/project">
+                Project
+              </NavLink>
+            </div>
+          )}
         </BrowserRouter>
       </div>
     );
